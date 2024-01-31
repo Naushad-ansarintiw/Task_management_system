e5f3ab34ae6fdb35d9b212c5ef9eb0eae3505dc8
Naushadansarinitw <naushad.nitw@gmail.com>
2024-01-31 22:55:26 +0530

    Done Database and Registration

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,50 +1,66 @@
 //handling error for async function
-const asyncHandler=require('express-async-handler')
+const asyncHandler = require('express-async-handler')
 
 // importing User model from models
-const User=require('../model/userSchema')
+const User = require('../model/userSchema')
 
 // registerUser Logic 
-const registerUser=asyncHandler(async(req,res)=>{
-    const {name,email,password,cpassword}=req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, role, password } = req.body;
+  console.log(email, role, password, "register");
 
-    if(!name||!email||!password||!cpassword){
-        res.status(400);
-        throw new Error("Please Enter all the Fields")
-    }
-    
-    const userExist= await User.findOne({email})
+  if (!role || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Fields")
+  }
+
+  const userExist = await User.findOne({ email })
 
 
-   if(userExist){
+  if (userExist) {
     res.status(400)
     throw new Error("user already exist ")
-   }
+  }
 
-   const user=await new User({
-    name,email,password,cpassword,
-   })
+  const user = await new User({
+    role, email, password,
+  })
 
-   const saveUser=await user.save();
+  const saveUser = await user.save();
 
 
-    if(saveUser){
-        res.status(201).json({
-            _id:saveUser._id,
-            name:saveUser.name,
-            email:saveUser.email,
-            password:saveUser.password,
-            cpassword:saveUser.cpassword,
-            pic:saveUser.pic,
-            token:generateToken(saveUser._id),
-        })
-            }
-        else{
-            res.status(400);
-            throw new Error("Failed to create the user")
-        }
-    }
+  if (saveUser) {
+    res.status(201).json({
+      _id: saveUser._id,
+      email: saveUser.email,
+      password: saveUser.password,
+      role: saveUser.role
+    })
+  }
+  else {
+    res.status(400);
+    throw new Error("Failed to create the user")
+  }
+}
 )
 
+const authUser = asyncHandler(async (req, res) => {
 
-module.exports={registerUser}
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && ((user.password == password))) {
+    res.json({
+      _id: user._id,
+      email: user.email,
+      role: user.role
+    })
+  }
+  else {
+    res.status(400)
+    throw new Error("Invalid Email or Password")
+  }
+})
+
+module.exports = { registerUser, authUser }
